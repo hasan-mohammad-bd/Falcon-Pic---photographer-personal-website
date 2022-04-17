547a539d65b50edcee4781287c4464184fa35417
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-17 23:40:50 +0600

    adding slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
+import "./Slider.css"
 
 const Slider = () => {
     const [index, setIndex] = useState(0);
@@ -10,36 +11,26 @@ const Slider = () => {
     return (
 <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <div className='img-slider-holder'></div>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <div class="cover1"></div>
+        <Carousel.Caption className='h-50'>
+          <h1 className='text-center heading'>Meet with your professional photographer </h1>
+          <button className='btn1'>Book Now</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
+      <div class="cover2"></div>
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Carousel.Caption className='h-50'>
+          <h1 className='text-center heading'>Seven years of fashion photography experience</h1>
+          <button className='btn1'>Book Now</button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+      <div class="cover3"></div>
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+      <Carousel.Caption className='h-50'>
+          <h1 className='text-center heading'>Using most latest camera technology and gears</h1>
+          <button className='btn1'>Book Now</button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
