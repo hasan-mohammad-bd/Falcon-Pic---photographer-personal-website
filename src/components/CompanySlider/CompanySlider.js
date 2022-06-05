fbeebcce3c6f1e98ac79720c06a7d20cb0cc24f1
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-06-05 23:35:23 +0600

    adding companySlider

---
+++ src/components/CompanySlider/CompanySlider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompanySlider = () => {
+    return (
+        <div>
+            This is the company slider
+        </div>
+    );
+};
+
+export default CompanySlider;
